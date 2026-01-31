9dff13fc8338c86e490054dfe9937ddb5bcbe11a
Mehrad44 <mehrad.langrud@outlook.com>
2026-01-31 17:32:54 +0330

    add api integration tests

---
+++ TicketingSystem.Tests/Integration/ApiFactory.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TicketingSystem.Tests.Integration
+{
+    public class ApiFactory : WebApplicationFactory<Program>
+    {
+    }
+}
---
+++ TicketingSystem.Tests/Integration/AuthTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http.Json;
+using System.Text;
+using FluentAssertions;
+using TicketingSystem.Application.Dtos.Auth;
+
+namespace TicketingSystem.Tests.Integration
+{
+    public class AuthTests : IClassFixture<ApiFactory>
+    {
+        private readonly HttpClient _client;
+
+        public AuthTests(ApiFactory factory)
+        {
+            _client = factory.CreateClient();
+        }
+
+        [Fact]
+        public async Task login_should_fail_with_invalid_user()
+        {
+            var response = await _client.PostAsJsonAsync(
+                "/api/auth/login",
+                new LoginRequestDto("x@test.com", "123")
+            );
+
+            response.IsSuccessStatusCode.Should().BeFalse();
+        }
+    }
+}
